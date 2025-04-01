a4f518a26f38892c9c84d88b92e45573a10664a8
anjan-in <anjansen30@gmail.com>
2025-04-01 13:20:25 +0530

    minor UI adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 
-// prohibit things
-document.addEventListener('contextmenu', function (e) {
-  e.preventDefault();
-});
+// // prohibit things
+// document.addEventListener('contextmenu', function (e) {
+//   e.preventDefault();
+// });
 
-document.addEventListener('keydown', function (e) {
-  // Prevent F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U, Ctrl+S
-  if (
-    e.key === 'F12' ||
-    (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
-    (e.ctrlKey && (e.key === 'U' || e.key === 'S'))
-  ) {
-    e.preventDefault();
-  }
-});
+// document.addEventListener('keydown', function (e) {
+//   // Prevent F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U, Ctrl+S
+//   if (
+//     e.key === 'F12' ||
+//     (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
+//     (e.ctrlKey && (e.key === 'U' || e.key === 'S'))
+//   ) {
+//     e.preventDefault();
+//   }
+// });
 
-document.addEventListener('dragstart', function (e) {
-  e.preventDefault();
-});
+// document.addEventListener('dragstart', function (e) {
+//   e.preventDefault();
+// });
